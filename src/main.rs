24550995c36cb9bb271052c26062de699ba098e1
Josef Kuchař <email@josefkuchar.cz>
2017-11-23 20:15:16 +0100

    Cleanup + render to disk

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ extern crate image;
 #[macro_use]
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{App, Arg, Error};
 use rand::distributions::{IndependentSample, Range};
 use piston_window::*;
 use opengl_graphics::{ OpenGL };
+use std::path::Path;
+use std::fs::File;
+use image::{ImageBuffer, RGBA, Rgba};
+use texture::Filter;
 
 struct World {
     ants: Vec<Ant>,
@@ -140,8 +144,95 @@ impl Ant {
     }
 }
 
+struct Application {
+    world: World,
+    ants: usize,
+    speed: usize,
+    zoom: f64,
+    canvas: ImageBuffer<Rgba<u8>, Vec<u8>>
+}
+
+impl Application {
+    fn run(&mut self) {
+        let opengl = OpenGL::V3_2;
+        let mut window: PistonWindow = WindowSettings::new("Piston Ant", [640, 480]).opengl(opengl).build().unwrap();
+        let mut texture = Texture::from_image(
+            &mut window.factory,
+            &self.canvas,
+            &TextureSettings::new().filter(Filter::Nearest)
+        ).unwrap();
+
+         while let Some(e) = window.next() {
+        
+            if let Some(_) = e.update_args() {
+                for _ in 0..self.speed {
+                    self.world.update();
+                }
+            }
+        
+            if let Some(_) = e.render_args() {
+            
+                for (index, tile) in self.world.grid.tiles.iter().enumerate() {
+
+                    let x = index % self.world.grid.size.x;
+                    let y = index / self.world.grid.size.x;
+                
+                    self.canvas.put_pixel(x as u32, y as u32, image::Rgba(*tile));
+                }
+
+                texture.update(&mut window.encoder, &self.canvas).unwrap();
+
+                window.draw_2d(&e, |c, g| {
+                    clear([0.0; 4], g);
+                    image(&texture, c.transform.scale(self.zoom, self.zoom), g);
+                });
+            }
+
+            e.mouse_scroll(|_, y| {
+                self.zoom += y;
+                if self.zoom < 1.0 {
+                    self.zoom = 1.0;
+                }
+            });
+        }
+    }
+
+    fn generate(&mut self, cycles: usize, basepath: &Path) {
+        self.canvas = image::ImageBuffer::new(self.world.grid.size.x as u32 * self.zoom as u32, self.world.grid.size.y as u32 * self.zoom as u32);
+
+        for cycle in 0..cycles {
+            for _ in 0..self.speed {
+                self.world.update();
+            }
+
+            for (index, tile) in self.world.grid.tiles.iter().enumerate() {
+                let x = index % self.world.grid.size.x;
+                let y = index / self.world.grid.size.x;
+
+                for i in 0..self.zoom as u32 {
+                    for j in 0..self.zoom as u32 {
+                        self.canvas.put_pixel((x as u32 * self.zoom as u32) + i, (y as u32 * self.zoom as u32) + j, image::Rgba(*tile));
+                    }
+                }
+            }
+
+            let path = basepath.join(format!("frame{}.png", cycle));
+            File::create(&path).unwrap();
+            
+            File::create(&path).unwrap();
+            image::save_buffer(&path, &self.canvas, self.world.grid.size.x as u32 * self.zoom as u32, self.world.grid.size.y as u32 * self.zoom as u32, RGBA(8)).unwrap();
+        }
+    }
+
+    fn add_ants(&mut self) {
+        for _ in 0..self.ants {
+            let ant = Ant::new(&self.world.grid.size);
+            self.world.add_ant(ant);
+        }
+    }
+}
+
 fn main() {
-    // Define parser
     let matches = App::new("piston-ant")
         .version("1.0.0")
         .about("Langton's ant implementation")
@@ -180,6 +271,13 @@ fn main() {
             .help("Number of ants")
             .default_value("5")
             .takes_value(true))
+        .arg(Arg::with_name("cycles")
+            .short("c")
+            .long("cycles")
+            .value_name("INTEGER")
+            .help("Number of cycles, only with generate option")
+            .default_value("0")
+            .takes_value(true))
         .get_matches();
 
     // Parse arguments
@@ -187,58 +285,27 @@ fn main() {
     let height = value_t_or_exit!(matches.value_of("height"), usize);
     let ants = value_t_or_exit!(matches.value_of("ants"), usize);
     let speed = value_t_or_exit!(matches.value_of("speed"), usize);
-    //let path = value_t_or_exit!(matches.value_of("path"));
+    let cycles = value_t_or_exit!(matches.value_of("cycles"), usize);
 
-    let mut world = World::new(width, height);
+    let mut app = Application {
+        world: World::new(width, height),
+        ants: ants,
+        speed: speed,
+        zoom: 2.0,
+        canvas: image::ImageBuffer::new(width as u32, height as u32)
+    };
 
-    for _ in 0..ants
-     {
-        let ant = Ant::new(&world.grid.size);
-        world.add_ant(ant);
-    }
+    app.add_ants();
 
-    let mut zoom = 1.0;
-    let opengl = OpenGL::V3_2;
-    let mut window: PistonWindow = WindowSettings::new("Piston Ant", [640, 480]).opengl(opengl).build().unwrap();
-    let mut canvas = image::ImageBuffer::new(width as u32, height as u32);
-    let mut texture = Texture::from_image(
-        &mut window.factory,
-        &canvas,
-        &TextureSettings::new().filter(texture::Filter::Nearest)
-    ).unwrap();
-
-    while let Some(e) = window.next() {
-        
-        if let Some(_) = e.update_args() {
-            for _ in 0..speed {
-                world.update();
-            }
+    if matches.value_of("generate").is_some() {
+        let path = matches.value_of("generate").unwrap();
+        if Path::new(path).exists() {
+            app.generate(cycles, Path::new(path));
+            std::process::exit(0);
+        } else {
+            Error::exit(&Error::with_description("Path is not valid", clap::ErrorKind::InvalidValue));
         }
-        
-        if let Some(_) = e.render_args() {
-            
-            for (index, tile) in world.grid.tiles.iter().enumerate() {
-
-                let x = index % world.grid.size.x;
-                let y = index / world.grid.size.x;
-                
-                canvas.put_pixel(x as u32, y as u32, image::Rgba(*tile));
-            }
-
-            texture.update(&mut window.encoder, &canvas).unwrap();
-
-            window.draw_2d(&e, |c, g| {
-                clear([0.0; 4], g);
-                image(&texture, c.transform.scale(zoom, zoom), g);
-            });
-        }
-
-        e.mouse_scroll(|_, y| {
-            zoom += y;
-
-            if zoom < 1.0 {
-                zoom = 1.0;
-            }
-        });
+    } else {
+        app.run();
     }
 }
